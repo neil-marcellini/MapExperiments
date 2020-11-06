806f982358206de133e1a282e4a3d32ab57367a6
neil-marcellini <neil.marcellini@gmail.com>
2020-11-06 12:51:17 -0800

    Basic map

--- MapExperiments/ContentView.swift
+++ MapExperiments/ContentView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region)
     }
 }
 
